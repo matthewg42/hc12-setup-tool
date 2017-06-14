9c4c316dcabbc5a3f6a31b2e8a50bbc33fc81935
Matthew Gates <matthewg42@gmail.com>
2017-06-14 12:58:03 +0100

    change default pins

--- firmware/Config.h
+++ firmware/Config.h
@@ -2,9 +2,9 @@
 
 #include <Arduino.h>
 
-#define HC12_SET        D1
-#define HC12_RX         D2
-#define HC12_TX         D3
+#define HC12_SET        D5
+#define HC12_RX         D6
+#define HC12_TX         D7
 #define HC12_TIMEOUT_MS 75
 #define DEFAULT_BAUD    9600
 
